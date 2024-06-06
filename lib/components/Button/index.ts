63683d47c95cc09487adf966929e45dddd8afcb1
Arman Abovyan <armani.abo@gmail.com>
2024-06-06 18:28:17 +0400

    first commit

---
+++ lib/components/Button/index.ts
@@ -0,0 +1,60 @@
+import { LitElement, html, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import React from 'react';
+import { createComponent } from '@lit/react';
+
+import styles from './styles.component.scss?inline';
+
+const TAG_NAME = 'si-button';
+
+@customElement(TAG_NAME)
+class SIButtonLit extends LitElement {
+    @property({ type: String }) variant: 'primary' | 'secondary' | 'secondary-gray' | 'tertiary' | 'tertiary-gray' = 'primary';
+    @property({ type: String }) size: 'sm' | 'md' | 'lg' | 'xl' | '2xl' = 'md';
+    @property({ type: Boolean }) link = false;
+    @property({ type: Boolean }) icon = false;
+
+    static styles = unsafeCSS(styles);
+
+    get buttonClasses() {
+        const variantClasses = {
+            primary: 'btn-primary',
+            secondary: 'btn-secondary',
+            'secondary-gray': 'btn-secondary-gray',
+            tertiary: 'btn-tertiary',
+            'tertiary-gray': 'btn-tertiary-gray',
+        };
+        const sizeClasses = {
+            sm: 'btn-sm',
+            md: 'btn-md',
+            lg: 'btn-lg',
+            xl: 'btn-xl',
+            '2xl': 'btn-2xl',
+        };
+        const linkClasses = this.link ? 'btn-link' : '';
+        const iconClasses = this.icon ? 'btn-icon' : '';
+
+        return `btn ${variantClasses[this.variant]} ${sizeClasses[this.size]} ${linkClasses} ${iconClasses}`;
+    }
+
+    render() {
+        return html`
+            <button class="${this.buttonClasses}">
+                <slot></slot>
+            </button>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        [TAG_NAME]: SIButtonLit;
+    }
+}
+
+export const SIButton = createComponent({
+    tagName: TAG_NAME,
+    elementClass: SIButtonLit,
+    react: React,
+    events: {},
+});
---
+++ lib/components/index.ts
@@ -0,0 +1,1 @@
+export { SIButton } from './Button'
